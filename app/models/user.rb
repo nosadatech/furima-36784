2a8149795c2ac42ed4302a5e1561580ea7476cfa
nosadatech <nmakida2010@gmail.com>
2021-09-21 18:34:55 +0900

    新規登録/ログイン画面の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+# has_many :items
+# has_many :orders
+
+  validates :nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birthday, presence: true
+
 end
