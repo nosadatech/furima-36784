f85a22fe41022f1237bf21f468394ad327bd74ff
nosadatech <nmakida2010@gmail.com>
2021-09-23 11:39:12 +0900

    userモデル、ビュー、itemコントローラー、ルーティング設定、単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
 # has_many :items
 # has_many :orders
 
-  validates :nickname, :lastname, :firstname, :lastname_kana, :firstname_kana, :birthday, presence: true
+  validates :nickname, :birthday, presence: true
+
+  # password 半角英数字混合
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "Include both letters and numbers"}
+
+  # lastname,firstname 全角（漢字・ひらがな・カタカナ）
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ , message: "Input full-width characters"} do
+    validates :lastname
+    validates :firstname
+  end
+
+  # lastname_kana,firstname_kana 全角（カタカナ）
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: "Input full-width katakana characters"} do
+    validates :lastname_kana
+    validates :firstname_kana
+  end
 
 end
